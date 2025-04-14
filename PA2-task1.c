52455eb284d77a2f8385cdc00e11cdd0b1366f1f
Ramtin Matin <151786626+ramtin-matin@users.noreply.github.com>
2025-04-13 20:05:35 -0400

    Update PA2-task1.c

--- PA2-task1.c
+++ PA2-task1.c
@@ -183,7 +183,7 @@ void run_server() {
         int bytes_received = recvfrom(server_fd, buf, MESSAGE_SIZE, 0,
                                       (struct sockaddr *)&client_addr, &client_len);
         if (bytes_received > 0) {
-    usleep(100);  // Add 100 microseconds delay per packet
+   usleep(1000);  // Add 100 microseconds delay per packet
     sendto(server_fd, buf, bytes_received, 0,
            (struct sockaddr *)&client_addr, client_len);
         }
